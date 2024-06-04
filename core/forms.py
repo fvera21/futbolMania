3b39321e0623aed4e4847b9228060954e6c561e5
IgnacioRiffop <ig.riffo@duocuc.cl>
2024-06-04 16:58:36 -0400

    formularios con form django

--- core/forms.py
+++ core/forms.py
@@ -6,3 +6,23 @@ class RegistroForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class EmpresaForm(forms.Form):
+    nombreEmpresa = forms.CharField(label='Nombre de la empresa', max_length=100)
+    direccion = forms.CharField(label='Dirección', max_length=200)
+    telefono = forms.CharField(label='Teléfono', max_length=15)
+    correo = forms.EmailField(label='Correo')
+    web = forms.URLField(label='Sitio Web')
+
+class ClienteForm(forms.Form):
+    nombrecliente = forms.CharField(label='Nombre del cliente', max_length=100)
+    nombreEmpresa = forms.CharField(label='Nombre de la empresa', max_length=100)
+    direccion = forms.CharField(label='Dirección', max_length=200)
+    telefono = forms.CharField(label='Teléfono', max_length=15)
+    correo = forms.EmailField(label='Correo')
+
+class EnvioForm(forms.Form):
+    metodoenvio = forms.CharField(label='Método de envío', max_length=100)
+    direccionenvio = forms.CharField(label='Dirección', max_length=200)
+    condicionesenvio = forms.CharField(label='Condiciones de envío', max_length=500)
+
--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import Group
 from django.core.paginator import Paginator
-from .forms import RegistroForm
+from .forms import *
 
 def index(request):
     return render(request, 'core/index.html')
@@ -22,4 +22,24 @@ def registro(request):
     return render(request, 'core/registro.html', data)
 
 def ordenCompra(request):
-    return render(request, 'core/ordenCompra.html')
+    if request.method == 'POST':
+        empresaForm = EmpresaForm(request.POST, prefix='empresaForm')
+        vendedorForm = EmpresaForm(request.POST, prefix='vendedorForm')
+        clienteForm = ClienteForm(request.POST, prefix='clienteForm')
+        envioForm = EnvioForm(request.POST, prefix='envioForm')
+
+        if empresaForm.is_valid() and vendedorForm.is_valid() and clienteForm.is_valid() and envioForm.is_valid():
+            # Haz algo con los datos de los formularios
+            pass
+    else:
+        empresaForm = EmpresaForm(prefix='empresaForm')
+        vendedorForm = EmpresaForm(prefix='vendedorForm')
+        clienteForm = ClienteForm(prefix='clienteForm')
+        envioForm = EnvioForm(prefix='envioForm')
+
+    return render(request, 'core/ordenCompra.html', {
+        'empresaForm': empresaForm,
+        'vendedorForm': vendedorForm,
+        'clienteForm': clienteForm,
+        'envioForm': envioForm
+    })
